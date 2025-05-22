1f1ed42cfa255dd3c41526722c04284cb5505ec8
mustafatoknak1881 <mtkmk1881@gmail.com>
2025-05-22 17:54:55 +0300

    swagger integration added for login endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -56,6 +56,40 @@ const userController = require('../controllers/user.controller')
  *           type: string
  *           format: date-time
  *           example: "2023-05-20T14:30:00Z"
+ *     LoginInput:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           example: kullanici123
+ *         password:
+ *           type: string
+ *           example: Sifre123!
+ *       example:
+ *         username: kullanici123
+ *         password: Sifre123!
+ * 
+ *     LoginResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: string
+ *           example: success
+ *         token:
+ *           type: string
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *         user:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: integer
+ *               example: 1
+ *             username:
+ *               type: string
+ *               example: kullanici123
  */
 
 /**
@@ -83,6 +117,41 @@ const userController = require('../controllers/user.controller')
  *         description: Kullanıcı zaten mevcut
  */
 router.post('/register', userController.create);
+
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Kullanıcı girişi ve JWT token alımı
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginInput'
+ *     responses:
+ *       200:
+ *         description: Başarılı giriş. JWT token döner.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       401:
+ *         description: Geçersiz kimlik bilgileri
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: fail
+ *                 message:
+ *                   type: string
+ *                   example: Invalid username or password
+ */
 router.post('/login', userController.login)
 router.post('/delete', userController.delete); // This is for only testing. I know this is no at given task.
 
